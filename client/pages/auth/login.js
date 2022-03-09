7c767bc273f9eccd0ed84dd455c9e3f2675a863e
Tasmia Rahman <tasmiata@gmail.com>
2022-03-09 20:26:28 +0200

    fix linting issue

--- client/pages/auth/login.js
+++ client/pages/auth/login.js
@@ -1,40 +1,37 @@
-import React, { useState } from "react";
-import { signIn, useSession, getProviders, getCsrfToken } from "next-auth/react";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import React, { useState } from 'react'
+import { signIn, useSession, getProviders, getCsrfToken } from 'next-auth/react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-import FormInput from "../../components/FormInput";
-import Button from "../../components/Button";
+import FormInput from '../../components/FormInput'
+import Button from '../../components/Button'
 
 const login = ({ providers, csrfToken }) => {
-  const { query } = useRouter();
+  const { query } = useRouter()
   const { data: session } = useSession()
 
+  const { error, success } = query
 
-  const { error, success } = query;
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+  const onSubmit = async event => {
+    event.preventDefault()
+    setLoading(true)
+    signIn('credentials', { username: email, password })
+  }
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    setLoading(true);
-    signIn("credentials", { username: email, password });
-  };
-
-  const errorMessage = error ? t(`authLogin.errors.${error}`) : null;
-  const showError = error && email.length === 0 && password.length === 0;
-  const showSuccess = success && email.length === 0 && password.length === 0;
-  const hasEmptyField = password.length === 0 || email.length === 0;
+  const errorMessage = error ? t(`authLogin.errors.${error}`) : null
+  const showError = error && email.length === 0 && password.length === 0
+  const showSuccess = success && email.length === 0 && password.length === 0
+  const hasEmptyField = password.length === 0 || email.length === 0
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full">
       <div className="box">
         <form onSubmit={onSubmit} className="mb-6 md:mb-8">
-          <h2 className="font-bold text-xl md:text-2xl mb-4">
-            Hypertube
-          </h2>
+          <h2 className="font-bold text-xl md:text-2xl mb-4">Hypertube</h2>
 
           <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
           <div className="mb-4">
@@ -57,59 +54,47 @@ const login = ({ providers, csrfToken }) => {
               autocomplete="current-password"
               onEnter={onSubmit}
             />
-            {showSuccess && (
-              <p className="text-green-600 text-sm mt-2 mb-2">{t(success)}</p>
-            )}
+            {showSuccess && <p className="text-green-600 text-sm mt-2 mb-2">{t(success)}</p>}
           </div>
 
-          <Button
-            loading={loading}
-            onClick={onSubmit}
-            disabled={hasEmptyField}
-            className="w-full"
-          >
+          <Button loading={loading} onClick={onSubmit} disabled={hasEmptyField} className="w-full">
             Login
           </Button>
-
         </form>
 
         <div className="flex items-center justify-center">
           <div className="text-xs sm:text-sm">
             Don't have an account?
             <Link href="/auth/register">
-              <a className="font-bold border-b-2 border-solid border-gray-300">
-                Create an account
-              </a>
+              <a className="font-bold border-b-2 border-solid border-gray-300">Create an account</a>
             </Link>
           </div>
         </div>
 
-        {Object.values(providers).map((provider) => (
-        <div className="flex items-center justify-center">
-          {/* <img src={`/logo-${provider.id}.png`} alt={provider.name} className="w-8 mb-5" /> */}
-          <button
-          key={provider.id}
-          className="bg-black hover:bg-green-700 text-white font-bold py-2 px-4 border-red-500 rounded-md"
-          onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-        >
-          Login with {provider.name}
-        </button>
-        </div>
-      ))}
-        </div>
+        {Object.values(providers).map(provider => (
+          <div className="flex items-center justify-center">
+            {/* <img src={`/logo-${provider.id}.png`} alt={provider.name} className="w-8 mb-5" /> */}
 
-   
-      
+            <button
+              key={provider.id}
+              className="bg-black hover:bg-green-700 text-white font-bold py-2 px-4 border-red-500 rounded-md"
+              onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+            >
+              Login with {provider.name}
+            </button>
+          </div>
+        ))}
       </div>
-  );
-};
+    </div>
+  )
+}
 
-export default login;
+export default login
 
 export async function getServerSideProps() {
-  const providers = await getProviders();
-  const csrfToken = await getCsrfToken();
+  const providers = await getProviders()
+  const csrfToken = await getCsrfToken()
   return {
-    props: { providers, csrfToken },
-  };
+    props: { providers, csrfToken }
+  }
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -25,5 +25,5 @@ export default function Home() {
         All right reserved by @team_hypertube
       </footer>
     </div>
-  );
+  )
 }
