9b57e5db70f938df824f918703797f1573166931
Pontus Andersson <pontus.andersson@siili.com>
2022-06-04 10:37:11 +0300

    Added some dummy controllers (just comments, no code yet) and packages that we will need.

---
+++ server/controllers/movieController.js
@@ -0,0 +1,15 @@
+  /*
+  Fields for movie DB:
+  imbd rating: float
+  year: int
+  genre: string
+  description: string
+  director: string
+  cast: actor array (IDs to other table?)
+  comments: comment table IDs
+  subtitles: langcodes?
+
+  Comments table:
+  comment_body: string
+  user: user id?
+  */
