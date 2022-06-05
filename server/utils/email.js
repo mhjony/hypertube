075bfcd632bca928f9dcf9d8059dc8884506af34
Pontus Andersson <panderss@c1r6p7.hive.fi>
2022-06-05 12:48:51 +0300

    Fixed e-mailing upon user registration.

--- server/utils/email.js
+++ server/utils/email.js
@@ -7,21 +7,18 @@ export const sendEmail = (email, token) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      // user: 'hive.web.branch',
-      // pass: 'hive.web.branch93'
-      //   type: 'OAuth2',
-      user: process.env.APP_EMAIL,
-      pass: process.env.APP_PASS,
-      //   clientId: appClientId,
-      //   clientSecret: appClientSecret,
-      //   refreshToken: appRefreshToken
+      type: 'OAuth2',
+      user: process.env.USER_EMAIL,
+      pass: process.env.USER_PASS,
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      refreshToken: REFRESH_TOKEN
     },
   });
   const mailOptions = {
-    from: "hive.web.branch@gmail.com",
     to: email,
     subject: "Activate Your Hypertube Account Now",
-    text: `Hello! Here is your account activation link. Please click the link to verify your account: http://localhost:8000/auth/registrationVerify?token=${token}`,
+    text: `Hello! Here is your account activation link. Please click the link to verify your account: http://localhost:8000/auth/registration-verify?token=${token}`,
   };
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
