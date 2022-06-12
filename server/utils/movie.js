ab6332a11050f3b2918c851a11ecb5c3bb6b769c
Pontus Andersson <pontus.andersson@siili.com>
2022-06-12 15:30:10 +0300

    Added start of movie search backend.

--- server/utils/movie.js
+++ server/utils/movie.js
@@ -1,3 +1,51 @@
 /*
 * Place movie stuff here.
 */
+import axios from 'axios';
+
+const buildMovieList = async (filters) => {
+  let movies = [];
+  let hasMore = true;
+  const params = {
+    limit: 20,
+    page: filters.page || 1,
+    /*minimum_rating: filters.imdb || 0,
+    genre: filters.genre,
+    sort_by: filters.sort_by,
+    order_by: filters.order_by,
+    query_term: filters.search || '',*/
+  };
+  console.log('In buildMovieList.');
+  try {
+    let res = await axios.get(`${process.env.TORRENT_API}`, { params });
+    const moviesData = res.data.data;
+    const movieList = res.data.data.movies;
+    console.log('Response: ');
+    console.log(res);
+
+    if (moviesData.movie_count === 0) {
+      console.log('Error, no movies found!');//throw notFoundError();
+    }
+    if (moviesData.movie_count <= params.limit * params.page) {
+      hasMore = false;
+      console.log('1');
+    }
+    if (movieList) {
+      console.log('2');
+      /*await Promise.all(
+        movieList.map(async (movie) => {
+          res = await axios.get(`http://www.omdbapi.com/?i=${movie.imdb_code}&apikey=${OMDB_KEY}`);
+          movie.thumbnail = res.data.Poster; // eslint-disable-line no-param-reassign
+        }),
+      );*/
+      movies = movieList//.map((movie) => filteredMovieData(movie));
+    }
+  } catch (e) {
+    return { movies: [], hasMore: false, e };
+  }
+  return { movies, hasMore };
+};
+
+export default {
+  buildMovieList,
+};
