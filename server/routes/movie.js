1abcd94216889c16db18f0b61364a334fc624759
Pontus Andersson <panderss@c3r5p1.hive.fi>
2022-08-13 16:00:40 +0300

    Reorganized routes.

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -4,13 +4,18 @@ import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/get-movie-list", [auth], movieController.getMovieList);
+
 
 // Router for movie comments
-router.get("/comments/:imdb_code", [auth], movieController.getMovieComments);
+router.get("/comments/:imdb_code" , [auth] , movieController.getMovieComments);
 router.post("/comment/add/:imdb_code", [auth], movieController.addComment);
+
+// Routes for getting movies
+//router.get("/get-movie-list", [auth], movieController.getMovieList);
 router.get("/get-movie-list", movieController.getMovieList);
 router.get("/get-single-movie", movieController.getSingleMovie);
+
+// Routes for downloading/playing movies
 //router.get('/:imdbCode/player/:token', inputValidator.validateToken, movieController.playMovie);
 router.get("/player", movieController.playMovie);
 router.get("/download", movieController.downloadMovie);
