3c6e5e8efdf36c323f7d7d49572403c8daf0366d
Pontus Andersson <panderss@c3r4p1.hive.fi>
2022-10-04 20:25:36 +0300

    Can get subs, but still needs work on how to process them.

--- server/utils/subtitlesAPI.js
+++ server/utils/subtitlesAPI.js
@@ -13,11 +13,35 @@ const OpenSubtitles = new OpenSubtitlesApi({
   ssl: true,
 });
 
-const downloadSubtitles = (subtitlesUrl, option) =>
+
+
+
+/* axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+}); */
+
+
+const downloadSubtitles = (/* subtitlesUrl, options */) => {
+
+	var options = {
+		method: 'GET',
+		url: 'https://api.opensubtitles.com/api/v1/subtitles',
+		headers: {'Content-Type': 'application/json', 'Api-Key': 'p8v7XyDIIBm6M8jJnR6nHuu2U7d3G6H2'}
+	};
+	
+	/* axios.request(options).then(function (response) {
+		console.log(response);
+	}).catch(function (error) {
+		console.error(error);
+	}); */
+
   new Promise((resolve) => {
-    axios
-      .get(subtitlesUrl, { responseType: "stream" })
+		console.log('In promise.');
+    axios.request(options)
       .then((response) => {
+				console.log(response);
         fs.mkdirSync(option.dir, { recursive: true });
         const file = fs.createWriteStream(`${option.dir}/subtitle.vtt`);
         response.data.pipe(file);
@@ -28,7 +52,36 @@ const downloadSubtitles = (subtitlesUrl, option) =>
       .catch(() => {
         resolve();
       });
-  });
+  }); 
+
+}
+
+const searchSubtitles = async (imdbCode) => {
+
+	var options = {
+		method: 'GET',
+		url: `https://api.opensubtitles.com/api/v1/subtitles?imdb_id=${imdbCode}`,
+		headers: {'Content-Type': 'application/json', 'Api-Key': 'p8v7XyDIIBm6M8jJnR6nHuu2U7d3G6H2'}
+	};
+
+	axios.request(options).then(function (response) {
+		console.log('Logging data:');
+		console.log(response.data);
+		console.log(response.data.data.length);
+		let results = [];
+		response.data.data.forEach((sub) => {
+			//console.log(sub.attributes.language);
+			//console.log(sub.attributes.release);
+			if (sub.attributes.language === 'en') {
+				results.push(sub);
+			}
+		})
+		console.log(results.length);
+		return(response.data);
+	}).catch(function (error) {
+		console.error(error);
+	});
+}
 
 const getSubtitles = async (imdb_code) => {
   try {
@@ -54,12 +107,17 @@ const getSubtitles = async (imdb_code) => {
     ];
 
     let subtitles = [];
-
-    if (fs.existsSync(parentDir)) {
+		console.log('Subtitles:');
+		console.log(subtitles);
+    /* if (fs.existsSync(parentDir)) {
+			console.log(`parentDir: ${parentDir}`);
       subtitles = options.reduce((accum, option) => {
+				console.log(option);
         if (fs.existsSync(option.dir)) {
+					
           accum.push(option.lang);
         }
+				console.log(`accum = ${accum}`);
         return accum;
       }, []);
     } else {
@@ -82,7 +140,37 @@ const getSubtitles = async (imdb_code) => {
       });
       const result = await Promise.all(promises);
       subtitles = result.filter((r) => !!r);
-    }
+    }*/
+		const searchResult = await searchSubtitles(imdb_code);
+		console.log('Logging results:');
+		searchResult.foreach((result) => {
+			console.log('Another subtitle:');
+			console.log(result);
+		})
+		
+		/* ({
+			sublanguageid: "eng, fin, ger, rus",
+			extensions: ["srt", "vtt"],
+			imdb_code: imdb_code,
+			limit: 1,
+		});  */
+		const promises = [];
+		options.forEach((option) => {
+			console.log('This crap works until here.');
+			if (
+				searchResult[option.lang] &&
+				searchResult[option.lang][0] &&
+				searchResult[option.lang][0].vtt
+			) {
+				console.log('And until here.');
+				const subtitlesUrl = searchResult[option.lang][0].vtt;
+				promises.push(downloadSubtitles(subtitlesUrl, option));
+			}
+		});
+		const result = await Promise.all(promises);
+		console.log('This crap works until here.');
+		subtitles = result.filter((r) => !!r);
+		console.log(subtitles);
     return subtitles;
   } catch (err) {
     return [];
