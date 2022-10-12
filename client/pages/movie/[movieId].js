73e4d16728684aa466df848c75e730b3a76b159e
Tasmia Rahman <tasmiata@gmail.com>
2022-10-12 18:57:43 +0300

    fix: remove logs

--- client/pages/movie/[movieId].js
+++ client/pages/movie/[movieId].js
@@ -25,17 +25,6 @@ const movieId = ({ movie }) => {
     objectPosition: 'center'
   }
 
-  const handleMoviePlay = movie => {
-    // setShowPlayer(true)
-
-    console.log('asd handleMoviePlay', movie)
-
-    // redirect to profile page
-    // window.location.href = `/profile`
-
-    // Open a modal onClick
-  }
-
   return (
     <div>
       {session ? (
@@ -49,23 +38,7 @@ const movieId = ({ movie }) => {
             <div className="absolute inset-y-28 md:inset-y-auto md:bottom-10 inset-x-4 md:inset-x-12 space-y-6 z-50">
               <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">{movie?.title}</h1>
               <div className="flex items-center space-x-3 md:space-x-5">
-                {/* <button
-                  className="text-xs md:text-base bg-[#f9f9f9] text-black flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]"
-                  onClick={() => handleMoviePlay(movie)}
-                >
-                  <PlayIcon className="h-6 md:h-8" />
-                  <span className="uppercase font-medium tracking-wide">Play</span>
-                </button> */}
                 <MoviePlayerModal movie={movie} />
-
-                <button
-                  className="text-xs md:text-base bg-black/30 text-[#f9f9f9] border border-[#f9f9f9] flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]"
-                  onClick={() => setShowPlayer(true)}
-                >
-                  <PlayIcon className="h-6 md:h-8 text-white" />
-
-                  <span className="uppercase font-medium tracking-wide">Trailer</span>
-                </button>
 
                 <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
                   <PlusIcon className="h-6" />
